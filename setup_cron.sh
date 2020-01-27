c38d3ecf90ca90e11c6791d454a00f5150548478
r3baix <r3b3x@protonmail.com>
2020-01-27 23:20:30 +0100

    Use absolute path in setup_cron.sh and DL hosts.txt

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-   if [ `id -u` -ne 0 ]; then
-      echo "This script can be executed only as root, Exiting.."
-      exit 1
-   fi
-
-case "$1" in
-   install|update)
-
-	CRON_FILE="/var/spool/cron/root"
-
-	if [ ! -f $CRON_FILE ]; then
-	   echo "cron file for root doesnot exist, creating.."
-	   touch $CRON_FILE
-	   /usr/bin/crontab $CRON_FILE
-	fi
-
-	grep -qi "update_hosts" $CRON_FILE
-	if [ $? != 0 ]; then
-	   echo "Updating cron job wget https://sebsauvage.net/hosts/hosts "
-           /bin/echo "5 0 * * * utils/wget_BL.sh" >> $CRON_FILE
-	fi
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+echo "Downloading https://sebsauvage.net/hosts/host"
+wget -q https://sebsauvage.net/hosts/hosts -O $dir_path/utils/hosts.txt
+echo "Done"
+echo "Updating cron job wget https://sebsauvage.net/hosts/hosts "
+line="5 0 * * * $dir_path/utils/wget_BL.sh"
+(echo "$line" ) | crontab -
+echo "Done"
